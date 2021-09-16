3a036f4a2f4e92088879946c782a24a417ee8bd8
Daniel Metzger <80478213+dametzge@users.noreply.github.com>
2021-09-16 16:04:25 +0200

    Update hvhostsetup.ps1

--- demos/nested-vms-in-virtual-network/hvhostsetup.ps1
+++ demos/nested-vms-in-virtual-network/hvhostsetup.ps1
@@ -33,3 +33,24 @@ cmd.exe /c "netsh routing ip add persistentroute dest=$($NatSubnet.NetworkAddres
 cmd.exe /c "netsh routing ip add persistentroute dest=$($VirtualNetwork.NetworkAddress) mask=$($VirtualNetwork.SubnetMask) name=""$($NIC2IP.InterfaceAlias)"" nhop=$($HyperVSubnet.HostAddresses[0])"
 
 Get-Disk | Where-Object -Property PartitionStyle -EQ "RAW" | Initialize-Disk -PartitionStyle GPT -PassThru | New-Volume -FileSystem NTFS -AllocationUnitSize 65536 -DriveLetter F -FriendlyName "Hyper-V"
+
+New-Item -Path "C:\" -Name "SCT" -ItemType Directory
+
+$url1 = "https://download.microsoft.com/download/8/5/C/85C25433-A1B0-4FFA-9429-7E023E7DA8D8/LGPO.zip"
+$url2 = "https://download.microsoft.com/download/8/5/C/85C25433-A1B0-4FFA-9429-7E023E7DA8D8/Windows%2010%20Version%201809%20and%20Windows%20Server%202019%20Security%20Baseline.zip"
+$output1 = "C:\SCT\LGPO.zip"
+$output2 = "C:\SCT\Baseline2019.zip"
+
+Invoke-WebRequest -Uri $url1 -OutFile $output1
+Invoke-WebRequest -Uri $url2 -OutFile $output2
+
+Expand-Archive -Path C:\SCT\LGPO.zip -DestinationPath C:\SCT\LGPO -Verbose
+Expand-Archive -Path C:\SCT\Baseline2019.zip -DestinationPath C:\SCT\Baseline2019 -Verbose
+
+Copy-Item "C:\SCT\LGPO\LGPO_30\LGPO.exe" -Destination "C:\SCT\Baseline2019\Local_Script\Tools"
+
+Set-ExecutionPolicy RemoteSigned
+
+cd C:\SCT\Baseline2019\Local_Script\
+
+.\BaselineLocalInstall.ps1 -WS2019NonDomainJoined
